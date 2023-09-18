a81fc2141e2feaecd3c25a5cdd7cbad1a79cc34a
Gabr1el20 <6327@holbertonstudents.com>
2023-09-18 12:58:50 -0700

    Fixed semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,10 +2,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-
-    console.log(data);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 });

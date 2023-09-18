15ea791f85cd927751b14ac52aff7fef460e40c0
Gabr1el20 <6327@holbertonstudents.com>
2023-09-18 13:09:40 -0700

    Testing some things

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
+fs.writeFile(process.argv[2], process.argv[3], (err) => {
   if (err) {
     console.log(err);
-    return;
   }
-  console.log(data);
 });

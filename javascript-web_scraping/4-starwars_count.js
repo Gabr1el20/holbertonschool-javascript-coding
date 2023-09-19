ff52be4811e9c95249769c832fc35e2fef2b3d95
Gabr1el20 <6327@holbertonstudents.com>
2023-09-19 09:39:41 -0700

    Task 4:???

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 const apiMovie = process.argv[2];
-const apiWedge = 'https://swapi-api.hbtn.io/api/people/18/';
 request.get(apiMovie, (err, response, body) => {
   if (err) {
     console.log(err);
-  }
-  request.get(apiWedge, (err, response, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const data = JSON.parse(body);
-    console.log(data.films.length);
+  };
+  let characterFilms = 0;
+  const movies = JSON.parse(body).results;
+  // we travel across each movie
+    movies.forEach(movie => {
+    movie.character.forEach(character => {
+        if (character.includes('/people/18')) characterFilms++;
+    });
   });
+  console.log(characterFilms);
 });

8032f5c9aaa8aff5154978cb5e3596539b6e0afe
Gabr1el20 <6327@holbertonstudents.com>
2023-09-19 09:41:58 -0700

    ssssemistandard

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,13 +4,13 @@ const apiMovie = process.argv[2];
 request.get(apiMovie, (err, response, body) => {
   if (err) {
     console.log(err);
-  };
+  }
   let characterFilms = 0;
   const movies = JSON.parse(body).results;
   // we travel across each movie
-    movies.forEach(movie => {
+  movies.forEach(movie => {
     movie.characters.forEach(character => {
-        if (character.includes('/people/18')) characterFilms++;
+      if (character.includes('/people/18')) characterFilms++;
     });
   });
   console.log(characterFilms);

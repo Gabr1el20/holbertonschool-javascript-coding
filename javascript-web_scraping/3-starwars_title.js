4d5209f653aa22753c180e61d37585383e65ce8b
Gabr1el20 <6327@holbertonstudents.com>
2023-09-19 09:18:09 -0700

    Task 3: fixed

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-
-request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (data) => {
-    console,log(data.title);
-})
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+request.get(apiUrl, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const movieData = JSON.parse(body);
+  console.log(movieData.title);
+});

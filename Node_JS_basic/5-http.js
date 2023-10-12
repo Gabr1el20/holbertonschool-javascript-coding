821210ce2fdc0631d65e1919f79e7236b28923f3
Gabr1el20 <6327@holbertonstudents.com>
2023-10-12 23:48:35 +0000

    lets'see

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -21,7 +21,7 @@ const app = http.createServer((req, res) => {
       })
       .catch((error) => {
         res.statusCode = 400;
-        res.write(`This is the list of our students\n${error}`);
+        res.write(`This is the list of our students\nCannot load the database`);
         res.end();
       });
   }

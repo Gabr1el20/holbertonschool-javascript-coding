a35ff4ebec3e747c7f8467ec64263f41193f177d
Gabr1el20 <6327@holbertonstudents.com>
2023-10-13 00:02:10 +0000

    Task 6

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+/* eslint-disable */
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School');
+});
+
+app.listen(port);
